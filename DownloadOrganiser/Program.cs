8e914564291ed13d0bc7318239907b074f3687ee
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2020-08-04 13:28:07 +0100

    Add files via upload

---
+++ DownloadOrganiser/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DownloadOrganiser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("C:\\Users\\507\\Downloads");
+            foreach (var item in files)
+            {
+                FileInfo fi = new FileInfo(item);
+
+                if (fi.Extension == ".exe" )
+                {
+
+                    File.Move(item ,@"C:\Users\507\Documents\exes\" + Path.GetFileName(item));
+
+                }
+
+                if (fi.Extension == ".jpg" || fi.Extension == ".png" || fi.Extension == ".jpeg")
+                {
+                    File.Move(item, @"C:\Users\507\Pictures\" + Path.GetFileName(item));
+                }
+
+            }
+
+        }
+    }
+}
