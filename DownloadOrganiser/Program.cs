2eacffc4c4a8d081b2eba8583157202154a1de4a
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2020-08-20 15:45:06 +0100

    Add files via upload

--- DownloadOrganiser/Program.cs
+++ DownloadOrganiser/Program.cs
@@ -12,21 +12,33 @@ namespace DownloadOrganiser
     {
         static void Main(string[] args)
         {
+            bool createExeFile = false;
+            string[] imageFiles = { ".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi" ,".png", ".gif", ".webp", ".tiff", ".tif",
+                ".psd", ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpx", ".jpm", ".mj2",
+                ".svg", ".svgz", ".ai", ".eps"};
+            string[] videoFiles = {".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg", ".mp4", ".m4p", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd" };
+
             string[] files = Directory.GetFiles($"C:\\Users\\{Environment.UserName}\\Downloads");
+
             foreach (var item in files)
             {
                 FileInfo fi = new FileInfo(item);
 
-                if (fi.Extension == ".exe" )
+                if (fi.Extension == ".exe")
                 {
 
-                    File.Move(item ,$@"C:\Users\{Environment.UserName}\Documents\exes\" + Path.GetFileName(item));
+                    File.Move(item ,$@"C:\Users\{Environment.UserName}\Documents\Exes\" + Path.GetFileName(item));
 
                 }
 
-                if (fi.Extension == ".jpg" || fi.Extension == ".png" || fi.Extension == ".jpeg")
+                if (imageFiles.Contains(fi.Extension))
                 {
                     File.Move(item, $@"C:\Users\{Environment.UserName}\Pictures\" + Path.GetFileName(item));
+                }
+
+                if (videoFiles.Contains(fi.Extension))
+                {
+                    File.Move(item, $@"C:\Users\{Environment.UserName}\Videos\" + Path.GetFileName(item));
                 }
 
             }
